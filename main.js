f57dae1763877b71dc5c619558528915a24849b5
gabriele <gabrielescodinu96@gmail.com>
2020-12-01 16:03:19 +0100

    commit 02

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ for (i = 0; i < 3; i++) {
   numeriCasuali.push(numeroRandom);
 };
 var messaggio = alert(numeriCasuali);
+console.log(numeriCasuali);
+
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta,
 // i numeri che ha visto precedentemente.
-var timer = 5;
 var listaUtente = [];
-var numeroUtente;
 
 var intervallo = setInterval(function(){
-  document.getElementById("timer").innerHTML = timer;
-  if (timer === 0) {
-  } else {
-    timer--;
-    clearInterval(intervallo);
-    for (i = 0; i < 3; i++) {
-      var numeroUtente = Number(prompt("Inserisci un numero"))
+  clearInterval(intervallo);
+  for (i = 0; i < 3; i++) {
+      var numeroUtente = Number(prompt("Inserisci un numero"));
       listaUtente.push(numeroUtente);
     }
-  }
-}, 1000);
-console.log(listaUtente);
+  console.log(listaUtente);
 
-// Una volta inseriti i 5 numeri, il software dice quanti
-// e quali numeri sono stati ricordati.
-// for (i = 0; i < numeriCasuali.length; i++) {
-//   if (numeroUtente === numeriCasuali[i])
-// };
+  // Una volta inseriti i 5 numeri, il software dice quanti
+  // e quali numeri sono stati ricordati.
+  var numeriGiusti = [];
+  for (i = 0; i < listaUtente.length; i++) {
+    if (numeriCasuali.includes(listaUtente[i])) {
+      numeriGiusti.push(listaUtente[i]);
+    };
+  };
+  console.log("hai inserito " + numeriGiusti.length + " numeri giusti");
+  console.log("questi numeri sono: " + numeriGiusti);
+}, 2000);
